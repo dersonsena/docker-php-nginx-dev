337b48f0aaa8db8756bfc49b1aad237db43e6a11
Kilderson Sena <dersonsena@gmail.com>
2021-12-28 11:04:36 -0300

    fixing xdebug params

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,4 +5,4 @@ nginxIniFile="/etc/nginx/conf.d/default.conf"
 # Nginx changes
 sed -i "s:root /var/www/html/public;*:root $NGINX_DOCUMENT_ROOT;:g" $nginxIniFile
 
-php-fpm -D; nginx -g "daemon off;"
+nginx -g "daemon off;"
